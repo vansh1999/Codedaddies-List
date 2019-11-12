8bf1ef3c542c87e8d904f094150bdea4c98ee495
vansh1999 <vansh.bhardwaj1999@gmail.com>
2019-11-13 00:06:17 +0530

    done with it

---
+++ my_app/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Search(models.Model):
+
+    search = models.CharField(max_length=100)
+    created = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return self.search
+
+    class Meta:
+        verbose_name_plural = 'searches'
+
+
---
+++ my_app/views.py
@@ -0,0 +1,65 @@
+from django.shortcuts import render
+from bs4 import BeautifulSoup
+import requests
+from requests.compat import quote_plus
+from . import models
+# Create your views here.
+
+
+BASE_CRAGSLIST_URL = 'https://delhi.craigslist.org/search/?query={}'
+BASE_IMAGE_URL = 'https://images.craigslist.org/{}_300x300.jpg'
+
+
+def home(request):
+
+    return render(request , 'base.html')
+
+def new_search(request):
+
+    search = request.POST.get('search')
+    final_url = BASE_CRAGSLIST_URL.format(quote_plus(search))
+    models.Search.objects.create(search=search)
+    response = requests.get(final_url)
+    data = response.text
+
+
+    soup = BeautifulSoup(data , features='html.parser')
+
+    post_listings = soup.find_all('li', {'class': 'result-row'})
+    # print(post_listings)
+
+    final_posting = []
+
+    for post in post_listings:
+
+        post_title = post.find(class_='result-title').text
+        post_url = post.find('a').get('href')
+        post_loc = post.find('span' , class_ = 'result-meta').text
+        post_location = post_loc.split('\n')[1]
+
+        if post.find(class_='result-image').get('data-ids'):
+            post_image_id = post.find(class_='result-image').get('data-ids').split(',')[0].split(':')[1]
+            post_image_url = BASE_IMAGE_URL.format(post_image_id)
+            print(post_image_url)
+        else:
+            post_image_url = 'https://craigslist.org/images/peace.jpg'
+
+
+
+        final_posting.append((post_title, post_url , post_location , post_image_url))
+
+
+
+
+
+
+    stuff_for_frontend = {
+
+        'search' : search ,
+        'final_posting' : final_posting,
+
+    }
+
+    return render(request , 'my_app/new_search.html' , stuff_for_frontend)
+
+
